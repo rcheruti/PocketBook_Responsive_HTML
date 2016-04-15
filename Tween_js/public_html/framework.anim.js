cb1baf6fcb655fb5d774d423b5d24846c8dff577
rcheruti <rcheruti@WGPH_WS4.wgph.com.br>
2016-04-15 11:10:25 -0300

    Subindo apenas.

--- Tween_js/public_html/framework.anim.js
+++ Tween_js/public_html/framework.anim.js
@@ -1,13 +1,21 @@
 
 (function(){
   
+  /*
   function _animacao(){
     requestAnimationFrame(_animacao);
     TWEEN.update();
   }
   requestAnimationFrame(_animacao);
-  
-  
+  */
+ console.log( fastdom );
+ 
+  fastdom.measure(function(){
+    console.log( 'aa', fastdom );
+  });
+  fastdom.measure(function(){
+    console.log( 'bb', fastdom );
+  });
   
   
 })();
